c5a4aa938e8d658d168e3cbccc5b2b6a4dfbd776
lunoxx <a.mese99@gmail.com>
2023-03-30 18:52:05 +0300

    Small adjustments

--- src/Data/offers.js
+++ src/Data/offers.js
@@ -1,7 +1,17 @@
 import Survey1Image from "./../Assets/Images/Surveys/iphone14.png";
 import Survey2Image from "./../Assets/Images/Surveys/13pro.webp";
+import Netflix from "./../Assets/Images/Surveys/netflix.webp";
 
 export const offersData = [
+    {
+      // id: 53321394
+      disabled:false,
+      title: "Netflix -- One year subscription",
+      description: "You can get one-year subscription to Netflix, for free. Join now!",
+      link: "https://smrturl.co/o/477275/53321394?s1=",
+      image: Netflix,
+      countries: "USA"
+    },
     {
       // id: 53295977
       disabled:false,
@@ -18,15 +28,6 @@ export const offersData = [
       description: "ConsumerDigitalSurvey offers us an iPhone 13 PRO for giveaway. Complete the data to register, using the button below.",
       link: "https://smrturl.co/o/477275/53223513?s1=",
       image: Survey2Image,
-      countries: "USA"
-    },
-    {
-      // id: 53223513
-      disabled:true,
-      title: "Come back soon",
-      description: "At the moment, we do not have an offer available for this place. We will arrange a partnership soon!",
-      link: "https://smrturl.co/o/477275/53223513?s1=",
-      image: '',
       countries: "USA"
     },
     {
